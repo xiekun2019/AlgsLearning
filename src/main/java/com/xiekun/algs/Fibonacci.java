945b27e5dc7fa0c4cb6d4297c01f36715a825f1a
KunXie <a418165301@163.com>
2021-03-11 10:48:46 +0800

    斐波那契数列 dp 数组的迭代解法

--- src/main/java/com/xiekun/algs/Fibonacci.java
+++ src/main/java/com/xiekun/algs/Fibonacci.java
@@ -13,6 +13,11 @@ public class Fibonacci {
         long endTime2 = System.currentTimeMillis();
         System.out.println("带备忘录的递归解法 fib(46) = " + result2 + " cost time: " + (endTime2 - startTime2) + "毫秒");
 
+        long startTime3 = System.currentTimeMillis();
+        int result3 = fib3(46);
+        long endTime3 = System.currentTimeMillis();
+        System.out.println("dp 数组的迭代解法 fib(46) = " + result2 + " cost time: " + (endTime3 - startTime3) + "毫秒");
+
 
     }
 
@@ -43,4 +48,21 @@ public class Fibonacci {
         memo[n] = helper(memo, n - 1) + helper(memo, n - 2);
         return memo[n];
     }
+
+    // 3. dp 数组的迭代解法
+    public static int fib3(int N) {
+        if (N < 1) return 0;
+        if (N == 1 || N == 2) return 1;
+        int[] dp = new int[100];
+        for (int i = 0; i < 100; i++) {
+            dp[i] = 0;
+        }
+        // base case
+        dp[1] = dp[2] = 1;
+        for (int i = 3; i <= N; i++)
+            dp[i] = dp[i - 1] + dp[i - 2];
+        return dp[N];
+    }
+
+
 }
