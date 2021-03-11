35fb6f102091ae6885eda4820187a7cd96f1192e
KunXie <a418165301@163.com>
2021-03-11 10:15:14 +0800

    暴力递归斐波那契数列

--- src/main/java/com/xiekun/algs/Fibonacci.java
+++ src/main/java/com/xiekun/algs/Fibonacci.java
@@ -1,4 +1,14 @@
 package com.xiekun.algs;
 
 public class Fibonacci {
+    public static void main(String[] args) {
+        int result = fib(20);
+        System.out.println("fib(2) = " + result);
+    }
+
+    // 1. 暴力递归
+    public static int fib(int N) {
+        if (N ==1 || N == 2) return 1;
+        return fib(N - 1) + fib(N -2);
+    }
 }
