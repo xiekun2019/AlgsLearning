d8245d0fc34e1e54d21bd2fc84ec3d3870745f9e
KunXie <a418165301@163.com>
2021-03-12 10:31:53 +0800

    92. 反转链表 II --- 迭代解法

--- src/main/java/com/xiekun/algs/ReverseLinkedList.java
+++ src/main/java/com/xiekun/algs/ReverseLinkedList.java
@@ -12,7 +12,7 @@ class ListNode {
 
 public class ReverseLinkedList {
     public ListNode successor = null;
-
+    // 反转链表 --- 递归
     public ListNode reverseBetween(ListNode head, int left, int right) {
         if(left == 1) {
             return reverseHeadToN(head, right);
@@ -33,7 +33,55 @@ public class ReverseLinkedList {
         return last;
     }
 
-    public static void main(String[] args) {
+    // 反转链表 --- 迭代
+    public ListNode reverseBetween2(ListNode head, int left, int right) {
+        ListNode fakeNode = new ListNode(-1);
+        fakeNode.next = head;
+        ListNode pre = fakeNode;
 
+        // 将 pre 移动到 left 位置的前一个位置
+        for(int i = 0; i < left - 1; i++){
+            pre = pre.next;
+        }
+        // 记录 left 位置的前一个位置
+        ListNode leftPreNode = pre;
+        // 记录 left 位置
+        ListNode leftNode = pre.next;
+
+        // 将 pre 移动到 right 位置
+        for(int i = left; i <= right; i++){
+            pre = pre.next;
+        }
+
+        // 记录 right 位置的后一个位置
+        ListNode rightNextNode = pre.next;
+
+        // 切断链表
+        leftPreNode.next = null;
+        pre.next = null;
+
+        leftPreNode.next = reverse(leftNode);
+        leftNode.next = rightNextNode;
+
+        return fakeNode.next;
+    }
+
+    // 反转链表
+    public ListNode reverse(ListNode head) {
+        ListNode pre = null;
+        ListNode k = head;
+
+        while(k != null){
+            ListNode next = k.next;
+            k.next = pre;
+            pre = k;
+            k = next;
+        }
+
+        return pre;
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
     }
 }
