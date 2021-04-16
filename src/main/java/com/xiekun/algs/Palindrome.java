215edc51a43c8f2b1f926301d3a755826c6bb3a4
KunXie <a418165301@163.com>
2021-04-16 11:39:52 +0800

    一些算法

--- src/main/java/com/xiekun/algs/Palindrome.java
+++ src/main/java/com/xiekun/algs/Palindrome.java
@@ -19,6 +19,30 @@ public class Palindrome {
         return s.substring(l+1, r);
     }
 
+    /**
+     * 给你一个字符串 s ，每一次操作你都可以在字符串的任意位置插入任意字符。
+     * 请你返回让 s 成为回文串的 最少操作次数 。
+     * 「回文串」是正读和反读都相同的字符串。
+     * @param s
+     * @return
+     */
+    public int minInsertions(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+
+        for (int i = n-2; i >= 0; i--) {
+            for (int j = i+1; j < n; j++) {
+                if(s.charAt(i) == s.charAt(j)){
+                    dp[i][j] = dp[i+1][j-1];
+                } else {
+                    dp[i][j] = Math.min(dp[i+1][j], dp[i][j-1]) + 1;
+                }
+            }
+        }
+
+        return dp[0][n-1];
+    }
+
     public static void main(String[] args) {
         String s = "babad";
 
